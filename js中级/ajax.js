656fb5bd45e2e25243831495ad50a341d37cd62f
fengchuanyu <fenchuanyu@163.com>
2020-11-29 14:59:15 +0800

    admin

--- js中级/ajax.js
+++ js中级/ajax.js
@@ -23,3 +23,27 @@ function ajaxGet(url, fun) {
         }
     }
 }
+
+function ajaxPost(url,data,fun){
+    //第一步创建 XMLHttpRequest 对象
+        var xmlhttp;
+        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {// code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        // 第二部向服务器发送请求
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("levelname="+data.levelname);
+
+        // 接受回调数据
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var obj = JSON.parse(xmlhttp.responseText)
+                fun(obj)
+            }
+        }
+}
