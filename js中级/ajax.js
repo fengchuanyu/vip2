458479369408b2d97515f91c088f30b995e49d09
fengchuanyu <fenchuanyu@163.com>
2020-11-29 11:06:03 +0800

    admin

---
+++ js中级/ajax.js
@@ -0,0 +1,25 @@
+
+
+function ajaxGet(url, fun) {
+    //第一步创建 XMLHttpRequest 对象
+    var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // 第二部向服务器发送请求
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    // 接受回调数据
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var obj = JSON.parse(xmlhttp.responseText)
+            // console.log(obj.userName);
+            fun(obj)
+        }
+    }
+}
