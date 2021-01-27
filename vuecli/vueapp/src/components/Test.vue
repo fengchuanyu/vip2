6ea2cd5394b59d8e9c9123713dc4e0cc2f320cf9
fengchuanyu <fenchuanyu@163.com>
2021-01-27 10:44:09 +0800

    admin

---
+++ vuecli/vueapp/src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+      <h1 class="myh1">测试组件 {{num}}</h1> 
+      <button @click="addHandle">add</button> 
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num:123
+        }
+    },
+    props:['msg'],
+    methods: {
+        addHandle(){
+            ++this.num
+        }
+    },
+}
+</script>
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
