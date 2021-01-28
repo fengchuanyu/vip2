9020b6c30d9bb5d25591ff9ce1e7a144ee6ee839
fengchuanyu <fenchuanyu@163.com>
2021-01-28 10:50:07 +0800

    admin

--- vuecli/vueapp/src/components/Test2.vue
+++ vuecli/vueapp/src/components/Test2.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         组件2
+        <h1>{{$store.state.count}}</h1>
     </div>
 </template>
--- vuecli/vueapp/src/views/Page2.vue
+++ vuecli/vueapp/src/views/Page2.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <h1>{{$store.state.count}}</h1>
+        <button @click="$store.commit('add',2)">add</button>
         <h1>这是页面二</h1>
         <h1>用户名:{{$route.query.username}}</h1>
     </div>
@@ -7,7 +9,14 @@
 <script>
 export default {
     created() {
-        console.log(this.$route.query.username)
+        // console.log(this.$route.query.username)
+        console.log(this.$store.state.count);
+    },
+    methods: {
+        addHandle(){
+            // ++this.$store.state.count
+            this.$store.commit('add',2)
+        }
     },
 }
 </script>
