bb412c0a8c55ad0a6e787afa6ecf7dedd31c56fa
fengchuanyu <fenchuanyu@163.com>
2021-01-29 09:30:13 +0800

    admin

--- vuecli/vueapp/src/views/Page3.vue
+++ vuecli/vueapp/src/views/Page3.vue
@@ -5,10 +5,20 @@
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
+    data() {
+        return {
+            dataList:[]
+        }
+    },
     created() {
-        console.log(this.$route);
-        console.log(this.$router);
+        // console.log(this.$route);
+        // console.log(this.$router);
+        axios.get("https://bird.ioliu.cn/v2?url=https://m.douban.com/rexxar/api/v2/subject_collection/tv_domestic/items?start=0&count=18")
+        .then((res)=>{
+            console.log(res);
+        })
     },
 }
 </script>
